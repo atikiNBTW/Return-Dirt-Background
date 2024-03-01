b9eeeaa7a9fe46356a0e417fcb897209986c738f
atikiNBTW <74009330+atikiNBTW@users.noreply.github.com>
2024-03-01 16:08:14 +0500

    Upload template

---
+++ src/main/java/com/atikinbtw/return_backgrounds/ReturnBackgrounds.java
@@ -0,0 +1,10 @@
+package com.atikinbtw.return_backgrounds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReturnBackgrounds implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
