fbf3a27643396477d07a050b89de4113aca6f2f7
atikinbtw <robinmusy12@gmail.com>
2024-05-08 18:31:48 +0500

    moved logic of handling menu and options in game to their respectful classes

--- src/main/java/com/atikinbtw/returndirtbackground/mixins/ScreenMixin.java
+++ src/main/java/com/atikinbtw/returndirtbackground/mixins/ScreenMixin.java
@@ -3,13 +3,10 @@ package com.atikinbtw.returndirtbackground.mixins;
 import com.atikinbtw.returndirtbackground.ReturnDirtBackground;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.option.OptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
--- src/main/java/com/atikinbtw/returndirtbackground/mixins/ingame/GameMenuScreenMixin.java
+++ src/main/java/com/atikinbtw/returndirtbackground/mixins/ingame/GameMenuScreenMixin.java
@@ -1,22 +1,30 @@
-package com.atikinbtw.returndirtbackground.mixins;
+package com.atikinbtw.returndirtbackground.mixins.ingame;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
+    @Shadow @Final private boolean showMenu;
+
     private GameMenuScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(method = "renderBackground", at = @At("TAIL"))
+    @Inject(method = "renderBackground", at = @At("HEAD"), cancellable = true)
     private void renderBackground(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        super.renderDarkening(context);
+        if (this.showMenu) {
+            this.renderInGameBackground(context);
+        }
+
+        ci.cancel();
     }
 }
