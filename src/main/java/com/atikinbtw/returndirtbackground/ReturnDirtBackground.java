b8b266fea75783df9139dea000bc3062ed2308dd
atikiNBTW <74009330+atikiNBTW@users.noreply.github.com>
2024-07-07 22:34:45 +0500

    cleanup v2

--- src/main/java/com/atikinbtw/returndirtbackground/ReturnDirtBackground.java
+++ src/main/java/com/atikinbtw/returndirtbackground/ReturnDirtBackground.java
@@ -4,12 +4,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ReturnDirtBackground implements ClientModInitializer {
     private static final Identifier OPTIONS_BACKGROUND_TEXTURE = Identifier.of("textures/block/dirt.png");
-    public static final Logger LOGGER = LoggerFactory.getLogger(ReturnDirtBackground.class);
     private static final MinecraftClient client = MinecraftClient.getInstance();
 
     @Override
