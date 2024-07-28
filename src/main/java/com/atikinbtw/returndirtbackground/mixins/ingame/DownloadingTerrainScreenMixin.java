6a9fb668f468c2ebe04420c46d61b88a20093218
atikinbtw <robinmusy12@gmail.com>
2024-07-28 15:18:07 +0500

    bump version and change end/nether loading screens

--- src/main/java/com/atikinbtw/returndirtbackground/mixins/ingame/DownloadingTerrainScreenMixin.java
+++ src/main/java/com/atikinbtw/returndirtbackground/mixins/ingame/DownloadingTerrainScreenMixin.java
@@ -3,42 +3,18 @@ package com.atikinbtw.returndirtbackground.mixins.ingame;
 import com.atikinbtw.returndirtbackground.ReturnDirtBackground;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(DownloadingTerrainScreen.class)
-public abstract class DownloadingTerrainScreenMixin extends Screen {
-    @Shadow @Final private DownloadingTerrainScreen.WorldEntryReason worldEntryReason;
-    @Shadow protected abstract Sprite getBackgroundSprite();
+public abstract class DownloadingTerrainScreenMixin {
 
-    protected DownloadingTerrainScreenMixin(Text title) {
-        super(title);
-    }
-
-    @Inject(at = @At("HEAD"), method = "renderBackground", cancellable = true)
-    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        switch (this.worldEntryReason) {
-            case OTHER: {
-                ReturnDirtBackground.renderBackgroundTexture(context);
-                break;
-            }
-            case NETHER_PORTAL: {
-                context.drawSprite(0, 0, -90, context.getScaledWindowWidth(), context.getScaledWindowHeight(), this.getBackgroundSprite());
-                break;
-            }
-            case END_PORTAL: {
-                context.fillWithLayer(RenderLayer.getEndPortal(), 0, 0, this.width, this.height, 0);
-            }
-        }
-
-        ci.cancel();
+    /**
+     * @author atikiNBTW
+     * @reason to render dirt image across all screen
+     */
+    @Overwrite
+    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
+        ReturnDirtBackground.renderBackgroundTexture(context);
     }
 }
